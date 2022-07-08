86378ed3a77b1b5a522ba418a2c399b9a500c5f7
Ashley Mays <ashleymays091@gmail.com>
2022-07-08 11:33:37 -0700

    Fix pressed key bug and map all keyboard inputs to notes

--- js/index.js
+++ js/index.js
@@ -40,28 +40,43 @@ document.getElementById("key-assist").addEventListener("click", function() {
 
 // press keys
 let keysObject = {
-    'z': {pitch: "C3", pressed: false, freq: 130.81},
-    'x': {pitch: "D3", pressed: false, freq: 146.83},
-    'c': {pitch: "E3", pressed: false, freq: 164.81},
-    'v': {pitch: "F3", pressed: false, freq: 174.61},
-    'b': {pitch: "G3", pressed: false, freq: 196.00},
-    'n': {pitch: "A3", pressed: false, freq: 220.00},
-    'm': {pitch: "B3", pressed: false, freq: 246.94},
-    ',': {pitch: "C4", pressed: false, freq: 261.63},
-    '.': {pitch: "D4", pressed: false, freq: 293.66},
-    '/': {pitch: "E4", pressed: false, freq: 329.63},
-    'q': {pitch: "F4", pressed: false, freq: 349.23},
-    'w': {pitch: "G4", pressed: false, freq: 392.00},
-    'e': {pitch: "A4", pressed: false, freq: 440.00},
-    'r': {pitch: "B4", pressed: false, freq: 493.88},
-    't': {pitch: "C5", pressed: false, freq: 523.25},
-    'y': {pitch: "D5", pressed: false, freq: 587.33},
-    'u': {pitch: "E5", pressed: false, freq: 659.25},
-    'i': {pitch: "F5", pressed: false, freq: 698.46},
-    'o': {pitch: "G5", pressed: false, freq: 783.99},
-    'p': {pitch: "A5", pressed: false, freq: 880.00},
-    '[': {pitch: "B5", pressed: false, freq: 987.77},
-    ']': {pitch: "C6", pressed: false, freq: 1046.5},
+    'z': {pitch: "C3", freq: 130.81},
+    's': {pitch: "C#3", freq: 138.59},
+    'x': {pitch: "D3", freq: 146.83},
+    'd': {pitch: "D#3", freq: 155.56},
+    'c': {pitch: "E3", freq: 164.81},
+    'v': {pitch: "F3", freq: 174.61},
+    'g': {pitch: "F#3", freq: 185.00},
+    'b': {pitch: "G3", freq: 196.00},
+    'h': {pitch: "G#3", freq: 207.65},
+    'n': {pitch: "A3", freq: 220.00},
+    'j': {pitch: "A#3", freq: 233.08},
+    'm': {pitch: "B3", freq: 246.94},
+    ',': {pitch: "C4", freq: 261.63},
+    'l': {pitch: "C#4", freq: 277.18},
+    '.': {pitch: "D4", freq: 293.66},
+    ';': {pitch: "D#4", freq: 311.13},
+    '/': {pitch: "E4", freq: 329.63},
+    'q': {pitch: "F4", freq: 349.23},
+    '2': {pitch: "F#4", freq: 369.99},
+    'w': {pitch: "G4", freq: 392.00},
+    '3': {pitch: "G#4", freq: 415.30},
+    'e': {pitch: "A4", freq: 440.00},
+    '4': {pitch: "A#4", freq: 466.16},
+    'r': {pitch: "B4", freq: 493.88},
+    't': {pitch: "C5", freq: 523.25},
+    '6': {pitch: "C#5", freq: 554.37},
+    'y': {pitch: "D5", freq: 587.33},
+    '7': {pitch: "D#5", freq: 622.25},
+    'u': {pitch: "E5", freq: 659.25},
+    'i': {pitch: "F5", freq: 698.46},
+    '9': {pitch: "F#5", freq: 739.99},
+    'o': {pitch: "G5", freq: 783.99},
+    '0': {pitch: "G#5", freq: 830.61},
+    'p': {pitch: "A5", freq: 880.00},
+    '-': {pitch: "A#5", freq: 932.33},
+    '[': {pitch: "B5", freq: 987.77},
+    ']': {pitch: "C6", freq: 1046.5},
 }
 
 // audio stuff
@@ -84,7 +99,7 @@ function play(key) {
     osc.type = "sine";
     osc.frequency.value = keysObject[key].freq;
     osc.start(0);
-    gainNode.gain.setValueAtTime(0.5, context.currentTime); // attack
+    gainNode.gain.setValueAtTime(0.25, context.currentTime); // attack
     gainNode.gain.linearRampToValueAtTime(0, context.currentTime + 2); // decay
     gainNodes[key] = gainNode;
     osc.connect(gainNodes[key]);
@@ -97,16 +112,14 @@ function mute(key) {
 
 document.addEventListener("keydown", function(e) {
     if (!e.repeat && findKey(e) == true) {
-        keysObject[e.key].pressed = true;
         play(e.key);
-        el.style.background = "var(--pressed-key-color)";
+        el.style.filter = "invert(0.5)";
     }
 })
 
 document.addEventListener("keyup", function(e) {
     if (findKey(e) == true) {
-        keysObject[e.key].pressed = false;
         mute(e.key);
-        el.style.background = "revert";
+        el.style.filter = "invert(0)";
     }
 })
