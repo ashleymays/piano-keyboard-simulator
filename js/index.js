a8ad3ba7fcb1dd9cbbe6d90b8a06035f3dce1793
Ashley Mays <ashleymays091@gmail.com>
2022-07-09 10:58:54 -0700

    Reduce cracking noise when stopping playback

--- js/index.js
+++ js/index.js
@@ -18,26 +18,23 @@ menuBtn.addEventListener("click", function() {
 
 
 
-let noteNames = document.getElementsByClassName("note-name");
+let notes = document.getElementsByClassName("note");
 
 // Label Notes
 document.getElementById("key-assist").addEventListener("click", function() {
     if (this.checked) {
-        for (let i = 0; i < noteNames.length; ++i) {
-            noteNames[i].style.fontSize = "1.7rem";
+        for (let i = 0; i < notes.length; ++i) {
+            notes[i].style.fontSize = "1.65rem";
         }
     }
     else {
-        for (let i = 0; i < noteNames.length; ++i) {
-            noteNames[i].style.fontSize = "0";
+        for (let i = 0; i < notes.length; ++i) {
+            notes[i].style.fontSize = "0";
         }
     }
 })
 
 
-// TODO: GET TONAL.JS, ADD FUNCTION TO GET FREQUENCY OF NOTE BY VALUE, CREATE MAP WITH (KEY: KEYBOARD_INPUT, VALUE: NOTE_VALUE (E.G. C4))
-// IN KEYBOARD EVENT LISTENER, IF THE INPUT EXISTS IN THE MAP, GET THE VALUE AND PASS IT INTO PLAY AND MUTE FUNCTIONS
-// FREQUENCY PART IN PLAY FUNCTION WILL USE TONAL.JS'S FREQ METHOD INSTEAD OF 'keysObject[pitch].freq'
 
 
 
@@ -47,45 +44,44 @@ let keysMap = new Map([
     ['z', "C3"],
     ['s', "C#3"],
     ['x', "D3"],
-    ['d', "D#3 "],
-    ['c', "E3 "],
-    ['v', "F3 "],
-    ['g', "F#3 "],
-    ['b', "G3 "],
-    ['h', "G#3 "],
-    ['n', "A3 "],
-    ['j', "A#3 "],
-    ['m', "B3 "],
-    [',', "C4 "],
-    ['l', "C#4 "],
-    ['.', "D4 "],
-    [';', "D#4 "],
-    ['/', "E4 "],
-    ['q', "F4 "],
-    ['2', "F#4 "],
-    ['w', "G4 "],
-    ['3', "G#4 "],
-    ['e', "A4 "],
-    ['4', "A#4 "],
-    ['r', "B4 "],
-    ['t', "C5 "],
-    ['6', "C#5 "],
-    ['y', "D5 "],
-    ['7', "D#5 "],
-    ['u', "E5 "],
-    ['i', "F5 "],
-    ['9', "F#5 "],
-    ['o', "G5 "],
-    ['0', "G#5 "],
-    ['p', "A5 "],
-    ['-', "A#5 "],
-    ['[', "B5 "],
-    [']', "C6 "],
+    ['d', "D#3"],
+    ['c', "E3"],
+    ['v', "F3"],
+    ['g', "F#3"],
+    ['b', "G3"],
+    ['h', "G#3"],
+    ['n', "A3"],
+    ['j', "A#3"],
+    ['m', "B3"],
+    [',', "C4"],
+    ['l', "C#4"],
+    ['.', "D4"],
+    [';', "D#4"],
+    ['/', "E4"],
+    ['q', "F4"],
+    ['2', "F#4"],
+    ['w', "G4"],
+    ['3', "G#4"],
+    ['e', "A4"],
+    ['4', "A#4"],
+    ['r', "B4"],
+    ['t', "C5"],
+    ['6', "C#5"],
+    ['y', "D5"],
+    ['7', "D#5"],
+    ['u', "E5"],
+    ['i', "F5"],
+    ['9', "F#5"],
+    ['o', "G5"],
+    ['0', "G#5"],
+    ['p', "A5"],
+    ['-', "A#5"],
+    ['[', "B5"],
+    [']', "C6"],
 ]);
 
 // audio stuff
 let context = new (window.AudioContext || window.webkitAudioContext)();
-let el;
 
 // find the right pitch by the keyboard input
 function isValidInput(input) {
@@ -93,39 +89,41 @@ function isValidInput(input) {
 }
 
 let gainNodes = [];
-
 function play(pitch) {
     let osc = context.createOscillator();
     let gainNode = context.createGain();
-    osc.type = "sine";
-    osc.frequency.value = keysObject[pitch].freq;
+    osc.type = "sine"
+    osc.frequency.value = Tonal.Note.freq(pitch);
     osc.start(0);
-    gainNode.gain.setValueAtTime(0.5, context.currentTime); // attack
-    gainNode.gain.linearRampToValueAtTime(0, context.currentTime + 2); // decay
+
+    // This defines the initial value.
+    gainNode.gain.value = 0.25;
+    gainNode.gain.exponentialRampToValueAtTime(0.0001, context.currentTime + 5);
+
     gainNodes[pitch] = gainNode; // store gain node to manipulate volume of key later (see mute function)
+    
     osc.connect(gainNodes[pitch]);
     gainNodes[pitch].connect(context.destination);
 }
 
 function mute(pitch) {
-    gainNodes[pitch].gain.setValueAtTime(0, context.currentTime); // mute
+    gainNodes[pitch].gain.setValueAtTime(gainNodes[pitch].gain.value, context.currentTime);
+    gainNodes[pitch].gain.exponentialRampToValueAtTime(0.0001, context.currentTime + 0.03);
 }
 
 // press key on keybaord -> play note
 document.addEventListener("keydown", function(e) {
     if (!e.repeat && isValidInput(e.key)) {
-        let note = keysMap.get(e.key);
-        play(note);
-        el.style.filter = "invert(0.5)"; // change color of key in piano
+        play(keysMap.get(e.key));
+        document.getElementById(keysMap.get(e.key)).style.filter = "invert(0.5)";
     }
 })
 
 // let go of key on keybaord -> stop playing note
 document.addEventListener("keyup", function(e) {
-    if (isValidKey(e.key)) {
-        let note = keysMap.get(e.key);
-        mute(note);
-        el.style.filter = "invert(0)"; // change background color of piano key
+    if (isValidInput(e.key)) {
+        mute(keysMap.get(e.key));
+        document.getElementById(keysMap.get(e.key)).style.filter = "invert(0)"
     }
 })
 
@@ -133,15 +131,18 @@ document.addEventListener("keyup", function(e) {
 document.activeElement.addEventListener("mousedown", function(e) {
     // user presses a piano key -> play the note and change the background color of the key
     if (e.target.tagName == "BUTTON") {
-        play(e.target.value);
-        e.target.style.filter = "invert(0.4)"
+        play(e.target.id);
+        e.target.style.filter = "invert(0.5)";
     }    
 })
 
-document.activeElement.addEventListener("mouseup", function(e) {
+document.activeElement.addEventListener("click", function(e) {
     // user lets go of a piano key -> mute the note and change the background color of the key
     if (e.target.tagName == "BUTTON") {
-        mute(e.target.value);
-        e.target.style.filter = "invert(0)"
-    }    
+        mute(e.target.id);
+        e.target.style.filter = "invert(0)";
+    }
 })
+
+
+//  TODO: FIX BUG WHERE THE FILTER STAYS ON AFTER PRESSING THE KEY AND MOVING THE MOUSE
