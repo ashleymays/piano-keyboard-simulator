6e11ac65d14eefab041cc40c419e7f54fbaad96d
Ashley Mays <ashleymays091@gmail.com>
2022-07-11 17:31:16 -0700

    Change pressed key color

--- js/script.js
+++ js/script.js
@@ -1,26 +1,9 @@
 let notes = document.getElementsByClassName("note");
+let soften = document.getElementById("soften");
 let sustain = document.getElementById("sustain");
 let keyAssist = document.getElementById("key-assist");
 let piano = document.getElementById("piano");
 let context = new (window.AudioContext || window.webkitAudioContext)();
-
-// load all audio files when page loads so that when the user presses keys the sounds start on time
-// store buffers in array to access piano sounds when user presses a key
-let buffers = [];
-window.addEventListener("load", function() {
-    for (let i = 0; i < notes.length; ++i) {
-        let request = new XMLHttpRequest();
-        request.open("GET", "./audio/" + notes[i].id + ".mp3");
-        request.responseType = "arraybuffer";
-        request.onload = function() {
-            let undecodedAudio = request.response;
-            context.decodeAudioData(undecodedAudio, (data) => buffers[notes[i].id] = data)
-        }
-    request.send();
-    }
-})
-
-// map keyboard input to notes
 let keysMap = new Map([
     ['q', "C3"],
     ['2', "Db3"],
@@ -61,6 +44,22 @@ let keysMap = new Map([
     ['/', "C6"],
 ]);
 
+// load all audio files when page loads so that when the user presses keys the sounds start on time
+// store buffers in array to access piano sounds when user presses a key
+let buffers = [];
+window.addEventListener("load", function() {
+    for (let i = 0; i < notes.length; ++i) {
+        let request = new XMLHttpRequest();
+        request.open("GET", "./audio/" + notes[i].id + ".mp3");
+        request.responseType = "arraybuffer";
+        request.onload = function() {
+            let undecodedAudio = request.response;
+            context.decodeAudioData(undecodedAudio, (data) => buffers[notes[i].id] = data)
+        }
+    request.send();
+    }
+})
+
 function isValidInput(input) {
     // find the right pitch by the keyboard input
     return keysMap.has(input) === true;
@@ -69,14 +68,22 @@ function isValidInput(input) {
 function playNote(pitch) {
     let playSound = context.createBufferSource();
     let gainNode = context.createGain();
-    playSound.buffer = buffers[pitch];   
-    gainNode.gain.value = 1.5;
+    playSound.buffer = buffers[pitch];  
+
+    if (soften.checked === true) {
+        gainNode.gain.setValueAtTime(0.4, context.currentTime);
+        gainNode.gain.exponentialRampToValueAtTime(0.85, context.currentTime + 0.35);
+    }
+    else {
+        gainNode.gain.setValueAtTime(1.5, context.currentTime);
+    }
+
     playSound.connect(gainNode);
     gainNode.connect(context.destination);
     playSound.start(context.currentTime);
 
     if (sustain.checked === false) {
-        gainNode.gain.linearRampToValueAtTime(0, context.currentTime + 0.6);
+        gainNode.gain.exponentialRampToValueAtTime(0.001, context.currentTime + 3);
     }
 }
 
@@ -85,13 +92,15 @@ document.addEventListener("keydown", function(e) {
     // the pressed key is used for input in the piano -> play note
     // !e.repeat makes sure that only the first keydown event when holding down on a certain key is taken into account
     if (!e.repeat && isValidInput(e.key)) {
-        playNote(keysMap.get(e.key));
-        document.getElementById(keysMap.get(e.key)).style.filter = "invert(0.5)"; // change color of key
+        let pitch = keysMap.get(e.key);
+        playNote(pitch);
+        document.getElementById(pitch).classList.add("key-bkg-color"); // change color of key
     }
 })
 
 document.addEventListener("keyup", function(e) {
-    document.getElementById(keysMap.get(e.key)).style.filter = "invert(0)"; // change color of key
+    let pitch = keysMap.get(e.key);
+    document.getElementById(pitch).classList.remove("key-bkg-color"); // change color of key
 })
 
 // Mouse Input
@@ -99,29 +108,26 @@ let isDown;
 document.activeElement.addEventListener("mousedown", function(e) {
     if (e.target.tagName === "BUTTON") {
         playNote(e.target.id);
-        e.target.style.filter = "invert(0.5)";
+        e.target.classList.add("key-bkg-color");
         isDown = true;
     }    
 })
 
 document.activeElement.addEventListener("mouseup", function(e) {
-    e.target.style.filter = "invert(0)";
+    e.target.classList.remove("key-bkg-color");
     isDown = false;
 })
 
 piano.addEventListener("mouseover", function(e) {
     if (e.target.tagName == "BUTTON" && isDown === true) {
         playNote(e.target.id);
-        e.target.style.filter = "invert(0.5)";
+        e.target.classList.add("key-bkg-color");
     }
 })
 
 piano.addEventListener("mouseout", function(e) {
-    e.target.style.filter = "invert(0)";
+    e.target.classList.remove("key-bkg-color");
 })
-
-
-
 
 
 
