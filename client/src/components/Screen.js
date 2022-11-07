bb2155904a08e641e71089821549dc0272e6bfce
Ashley Mays <ashleymays091@gmail.com>
2022-11-07 02:00:10 -0800

    Add raw code to play a single note

--- client/src/components/Screen.js
+++ client/src/components/Screen.js
@@ -1,38 +1,56 @@
 import { useState, useEffect } from "react";
 
+
 function Screen() {
     const [instrument, setInstrument] = useState("Acoustic Grand");
 
-    useEffect(() => {
+    const play = () => {
         // get instrument sounds
         fetch("http://localhost:5000/audio", {
-                method: 'post',
-                headers: new Headers({ 
-                    "Access-Control-Allow-Origin": "http://localhost:5000",
-                    "Accept": "application/json", 
-                    "Content-Type": "application/json",
-                }),
-                body: JSON.stringify({ instrument: instrument }),
-                mode: 'cors'
+            method: 'post',
+            headers: new Headers({ 
+                "Access-Control-Allow-Origin": "http://localhost:5000",
+                "Accept": "application/json", 
+                "Content-Type": "application/json",
+            }),
+            body: JSON.stringify({ instrument: instrument }),
+            mode: 'cors'
+        })
+            .then(res => res.json())
+            .then(tree => {
+                // CONVERTING BASE64 STRING TO ARRAY BUFFER
+                let buffer;
+                const context = new AudioContext();
+                let req = new XMLHttpRequest;
+                req.open('GET', "data:application/octet;base64," + tree["C3.mp3"]);
+                req.responseType = 'arraybuffer';
+                req.onload = () => {
+                    let undecodedAudio = req.response;
+                    context.decodeAudioData(undecodedAudio, (data) => {
+                        let dest = context.createMediaStreamDestination();
+                        let gainNode = context.createGain();
+                        let bufferSource = context.createBufferSource();
+                        bufferSource.buffer = data;
+                        gainNode.gain.setValueAtTime(1.5, context.currentTime);
+                        gainNode.gain.exponentialRampToValueAtTime(0.001, context.currentTime + 10);
+                        bufferSource.connect(gainNode);
+                        gainNode.connect(dest);
+                        gainNode.connect(context.destination);
+                        bufferSource.start(context.currentTime);
+                        console.log(buffer)
+                    })
+                }
+                req.send();
             })
-                .then(res => res.json())
-                .then(tree => {
-                    // CONVERTING BASE64 STRING TO ARRAY BUFFER
-                    // convert base64 string to string of 
-                    let str = window.atob(tree["Ab1.mp3"])
+            .catch(err => console.error(err))
+    }
 
-                    // convert to array
-                    let arr = Uint8Array.from(str);
-
-                    // get array buffer
-                    let buffer = arr.buffer;
-                    console.log(buffer)
-                })
-                .catch(err => console.error(err))
+    useEffect(() => {
+        
     }, [instrument])
 
     return (
-        <div className="screen" onClick={() => setInstrument("Electric Piano")}></div>
+        <div className="screen" onClick={play}></div>
     )
 }
 
