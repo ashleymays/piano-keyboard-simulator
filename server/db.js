eb753e6f0363368d16dd5fa6504524a7b733b562
Ashley Mays <ashleymays091@gmail.com>
2023-01-16 21:48:32 -0800

    Removed database files

--- server/db.js
+++
@@ -1,6 +0,0 @@
-const mysql = require('mysql2');
-const credentials = require('./credentials');
-
-const connection = mysql.createConnection(credentials);
-
-module.exports = connection;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const readFileTree = require('read-file-tree');
-const connection = require('./db');
 const app = express();
 
 app.use(cors())
@@ -11,47 +10,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 
-connection.connect((err) => {
-    if (err) throw err;
-
-    console.log('Connected to database');
-
-    // Get audio files for a certain instrument from the directory
-    app.post('/audio', (req, res) => {
-        const filePath = path.resolve(__dirname, "../client", "public", "audio", req.body.instrument);
-        readFileTree(filePath, { encoding: "base64" }, (err, tree) => {
-            if (err) throw err;
-            res.json(tree)
-        })
-    })
-
-
-    // Add a recording to the database
-    app.post('/recording', (req, res) => {
-        let filePath = path.resolve(__dirname, '../client/public/userAudio', req.body.title + '.mp3');
-        let recordingTitle = req.body.title;
-        let sql = `INSERT INTO RECORDINGS (TITLE, FILEPATH)
-                   VALUES ('${recordingTitle}', '${filePath}')`
-
-        console.log(req.body.fileData)
-        res.json({})
-        
-        // connection.query(sql, (err, results, fields) => {
-        //     if (err) throw err;
-        //     res.json(req.body);
-        // })
-    })
-
-    // Get all recordings from a database
-    app.get('/recording', (req, res) => {
-        let sql = `SELECT * FROM RECORDINGS`;
-        connection.query(sql, (err, results, fields) => {
-            if (err) throw err;
-            res.json(results);
-        })
+// Get audio files for a certain instrument from the directory
+app.post('/audio', (req, res) => {
+    const filePath = path.resolve(__dirname, "../client", "public", "audio", req.body.instrument);
+    readFileTree(filePath, { encoding: "base64" }, (err, tree) => {
+        if (err) throw err;
+        res.json(tree)
     })
 })
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
