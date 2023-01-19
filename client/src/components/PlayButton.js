ad5768c4638743edbf54be646a23bf8eca3090d2
Ashley Mays <ashleymays091@gmail.com>
2023-01-18 19:18:18 -0800

    Split PlayButton component off of RecordingControls component

---
+++ client/src/components/PlayButton.js
@@ -0,0 +1,41 @@
+import Button from "./Button";
+import { useEffect, useState, useRef } from "react";
+
+
+function PlayButton(props) {
+    const [shouldPlayRecording, setShouldPlayRecording] = useState(false);
+    const [recordingURL, setRecordingURL] = useState('');
+    let recordingAudio = useRef(null);
+
+    const mediaRecorder = props.mediaRecorder;
+
+    const createRecordingURL = () => {
+        let blob = new Blob(recordingAudio.current, { type: 'audio/mp3' });
+        let url = URL.createObjectURL(blob);
+        setRecordingURL(url);
+    }
+
+    mediaRecorder.ondataavailable = (e) => {
+        recordingAudio.current = [e.data];
+        createRecordingURL();
+    }
+
+    // Play the most recent recording
+    useEffect(() => {
+        const audioPlayer = document.getElementById('recording-audio-player');
+        if (shouldPlayRecording) {
+            audioPlayer.play();
+        } else {
+            audioPlayer.pause();
+        }
+    }, [shouldPlayRecording])
+
+    return (
+        <>
+            <audio hidden id="recording-audio-player" src={recordingURL} type="audio/mp3"></audio>
+            <Button type="checkbox" className="round-btn" onChange={() => setShouldPlayRecording(!shouldPlayRecording)}>Play</Button>
+        </>
+    )
+}
+
+export default PlayButton;
