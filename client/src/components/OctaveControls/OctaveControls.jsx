d3a9953462df77c86e000c47cb6303e5df2eaaf5
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2023-10-20 03:43:44 +0000

    edit octave control css

--- client/src/components/OctaveControls/OctaveControls.jsx
+++ client/src/components/OctaveControls/OctaveControls.jsx
@@ -2,11 +2,17 @@ function OctaveControls() {
   return (
     <div className="octave-controls flex-column align-items-center">
       <div className="octave-controls__btns flex-column align-items-center justify-content-space-btwn">
-        <button type="button" className="octave-controls__btn-arrow">
+        <button
+          type="button"
+          className="octave-controls__btn-arrow flex-column"
+        >
           &uarr;
         </button>
         <div className="octave-controls__line" />
-        <button type="button" className="octave-controls__btn-arrow">
+        <button
+          type="button"
+          className="octave-controls__btn-arrow flex-column justify-content-end"
+        >
           &darr;
         </button>
       </div>
