ccd900073e5c4fdc0f7ecde6bcf9d4e729cf42f7
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2023-10-29 01:00:58 +0000

    Update 2 files

--- client/src/components/PianoKeys/PianoKeys.functions.js
+++ client/src/components/PianoKeys/PianoKeys.functions.js
@@ -113,7 +113,6 @@ export function endNote(event) {
         const pitch = getPitchByComputerKey(computerKey);
         removePianoKeyColor(computerKey);
         removeComputerKeyFromPressedKeysArray(computerKey);
-        endNoteAtPitch(pitch);
     }
 }
 
@@ -144,17 +143,6 @@ function removePianoKeyColor(computerKey) {
 function removeComputerKeyFromPressedKeysArray(computerKey) {
     const index = currentlyPressedKeys.indexOf(computerKey);
     currentlyPressedKeys.splice(index, 1);
-}
-
-/**
- * Lowers the volume of the gain node down to imperceptible volume after a second.
- * Makes the note ending sound natural.
- * @param { string }
- */
-function endNoteAtPitch(pitch) {
-    const NOTE_DURATION = 0.2;
-    const currentNoteGainNode = gainNodesUsed[pitch];
-    currentNoteGainNode.gain.setValueAtTime(0.01, audioContext.currentTime + NOTE_DURATION);
 }
 
 /**
