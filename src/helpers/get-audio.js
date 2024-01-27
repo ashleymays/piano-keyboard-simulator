d598a92ba9daf78172fe2247b3ffe99f8590b142
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2024-01-27 05:14:23 +0000

    chore: use function declarations instead of expressions in get-audio file and add jsdoc

--- src/helpers/get-audio.js
+++ src/helpers/get-audio.js
@@ -1,7 +1,21 @@
-/* eslint require-jsdoc: 0 */
 import { ToneAudioBuffers } from 'tone';
 
-const getAudioFilePaths = (directory) => {
+/**
+ * Returns the audio buffers for a specific instrument.
+ * @param { string } directory 
+ * @returns { Promise<ToneAudioBuffers> }
+ */
+export function getAudioBuffers(directory) {
+  const filePaths = getAudioFilePaths(directory);
+  return getToneConstructorAsPromise(ToneAudioBuffers, filePaths);
+};
+
+/**
+ * Returns the paths to the audio for a specific instrument.
+ * @param { string } directory the folder where the instrument's audio is stored
+ * @returns { object }
+ */
+function getAudioFilePaths(directory) {
   // prettier-ignore
   const notes = ['A', 'Ab', 'B', 'Bb', 'C', 'D', 'Db', 'E', 'Eb', 'F', 'G', 'Gb'];
   const octaves = [1, 2, 3, 4, 5, 6, 7];
@@ -20,15 +34,16 @@ const getAudioFilePaths = (directory) => {
   return filePaths;
 };
 
-const getToneConstructorAsPromise = (ToneConstructor, options) => {
+/**
+ * Wraps a constructor from Tone.js in a awaitable Promise.
+ * @param { ToneAudioBuffers } ToneConstructor 
+ * @param { object } options 
+ * @returns { Promise<ToneAudioBuffers> }
+ */
+function getToneConstructorAsPromise(ToneConstructor, options) {
   return new Promise((resolve, reject) => {
     const results = new ToneConstructor(options, (error) =>
       error ? reject(error) : resolve(results)
     );
   });
-};
-
-export const getAudioBuffers = (directory) => {
-  const filePaths = getAudioFilePaths(directory);
-  return getToneConstructorAsPromise(ToneAudioBuffers, filePaths);
 };
