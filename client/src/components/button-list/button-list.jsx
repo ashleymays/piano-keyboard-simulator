364a9fae79cf37580c715aadf54ebf67c57f9aa6
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2024-01-27 19:03:40 +0000

    BREAKING CHANGE remove fetching audio files code in client

--- client/src/components/button-list/button-list.jsx
+++ client/src/components/button-list/button-list.jsx
@@ -1,41 +1,12 @@
-import { useState, useEffect, useContext } from 'react';
-import { MainContext } from '~/context';
 import { Button } from '~/components/button';
-import { getAudioBuffers } from './button-list.helpers';
 import { instruments } from '~/data';
 
 export function ButtonList() {
-  const [directory, setDirectory] = useState(instruments[0].directory);
-
-  const { buffers } = useContext(MainContext);
-
-  const setBuffers = async () => {
-    console.log('called');
-    if (!buffers.current[directory]) {
-      buffers.current[directory] = await getAudioBuffers(directory);
-    }
-    buffers.current._active = directory;
-  };
-
-  const setAudio = async () => {
-    try {
-      await setBuffers();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    setAudio();
-  }, [directory]);
-
   return (
     <div className="instrument-btn-list">
       {instruments.map((instrument) => (
         <Button
           key={instrument.directory}
-          onClick={() => setDirectory(instrument.directory)}
-          isActive={instrument.directory === directory}
           {...instrument}
         />
       ))}
