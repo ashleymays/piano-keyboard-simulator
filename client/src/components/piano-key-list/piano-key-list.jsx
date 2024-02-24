d6952c900d6ef2e282c67443c78aede12b446d7c
Ashley Mays <ashleymays091@gmail.com>
2024-02-24 11:06:19 -0800

    refactor (piano-key-list): move piano key components inside list

--- client/src/components/piano-key-list/piano-key-list.jsx
+++ client/src/components/piano-key-list/piano-key-list.jsx
@@ -1,19 +1,17 @@
 import { PianoKey } from '~/components/piano-key';
 import { pianoKeys } from '~/data';
 
-const pianoKeyComponents = (() => {
-  const components = [];
-  pianoKeys.forEach((pianoKey) =>
-    components.push(
-      <PianoKey
-        key={pianoKey.computerKey}
-        {...pianoKey}
-      />
-    )
-  );
-  return components;
-})();
-
 export function PianoKeyList() {
-  return <div className="piano-key-list">{pianoKeyComponents}</div>;
+  const pianoKeysArray = Array.from(pianoKeys.values());
+
+  return (
+    <div className="piano-key-list">
+      {pianoKeysArray.map((pianoKey) => (
+        <PianoKey
+          key={pianoKey.computerKey}
+          {...pianoKey}
+        />
+      ))}
+    </div>
+  );
 }
