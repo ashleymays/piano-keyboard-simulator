94faeb657cffdbf48a9d9e6990ef5e9d95422721
Ashley Mays <ashleymays091@gmail.com>
2024-03-09 13:18:29 -0800

    fix (piano-keys): add pressed piano key color

--- client/src/components/piano-keys/use-piano-keys.helpers.js
+++ client/src/components/piano-keys/use-piano-keys.helpers.js
@@ -10,6 +10,7 @@ export function endNote(event) {
 
   if (index !== -1) {
     currentlyPlayedKeys.splice(index, 1);
+    removePressedPianoKeyColor(computerKey);
   }
 }
 
@@ -23,8 +24,11 @@ export function playNote(event, audioSamples) {
   const computerKey = getComputerKeyByEvent(event);
   const pitch = getPitchByComputerKey(computerKey);
 
+  console.log(event.target);
+
   if (pitch && !currentlyPlayedKeys.includes(computerKey)) {
     playNoteAtPitch(pitch, audioSamples);
+    addPressedPianoKeyColor(computerKey);
     currentlyPlayedKeys.push(computerKey);
   }
 }
@@ -54,4 +58,30 @@ function playNoteAtPitch(pitch, audioSamples) {
   const player = new Player().toDestination();
   player.buffer = audioSamples.get(pitch);
   player.start();
+}
+
+/**
+ * Sets the background color of the piano key being played.
+ * Needed for the keydown event.
+ *
+ * @param {string}
+ */
+function addPressedPianoKeyColor(computerKey) {
+  const pianoKeyElement = document.querySelector(
+    `button[value="${computerKey}"]`
+  );
+  pianoKeyElement.classList.add('pressed-piano-key');
+}
+
+/**
+ * Resets the background color of the piano key being played.
+ * Needed for the keyup event.
+ *
+ * @param {string}
+ */
+function removePressedPianoKeyColor(computerKey) {
+  const pianoKeyElement = document.querySelector(
+    `button[value="${computerKey}"]`
+  );
+  pianoKeyElement.classList.remove('pressed-piano-key');
 }
