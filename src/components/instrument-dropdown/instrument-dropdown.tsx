e974401fdcd5bdcd83a1b8acfbd65b5f6f264d70
Ashley Mays <ashleymays091@gmail.com>
2024-06-11 17:48:00 -0700

    feat: add hook to set instrument

--- src/components/instrument-dropdown/instrument-dropdown.tsx
+++ src/components/instrument-dropdown/instrument-dropdown.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
 import Dropdown from 'react-dropdown';
+import { useInstrument } from './use-instrument';
 import { UpArrowIcon, DownArrowIcon } from '~/components/arrow-icon';
 
 const options = [
@@ -16,8 +16,10 @@ const options = [
   { value: '8-bit', label: '8-Bit', className: 'instrument-dropdown__option' }
 ];
 
+const defaultInstrument = options[0].value;
+
 export const InstrumentDropdown = () => {
-  const [instrument, setInstrument] = useState(options[0].value);
+  const [instrument, handleInstrument] = useInstrument({ defaultInstrument });
 
   return (
     <Dropdown
@@ -28,6 +30,7 @@ export const InstrumentDropdown = () => {
       menuClassName="instrument-dropdown__options"
       arrowClosed={<DownArrowIcon />}
       arrowOpen={<UpArrowIcon />}
+      onChange={({ value }) => handleInstrument(value)}
     />
   );
 };
---
+++ src/components/instrument-dropdown/use-instrument.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+type UseInstrumentProps = {
+  defaultInstrument: string;
+};
+
+export const useInstrument = ({ defaultInstrument }: UseInstrumentProps) => {
+  const [instrument, setInstrument] = useState(defaultInstrument);
+
+  const handleInstrument = (newInstrument: string) => {
+    setInstrument(newInstrument);
+  };
+
+  return [instrument, handleInstrument];
+};
