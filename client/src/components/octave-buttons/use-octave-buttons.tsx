11c99a66f88a078ff112ea5cd7d91c9083af2216
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2024-06-19 19:21:10 -0700

    refactor (use-octave-buttons): move functions outside of hook

--- client/src/components/octave-buttons/use-octave-buttons.tsx
+++ client/src/components/octave-buttons/use-octave-buttons.tsx
@@ -8,22 +8,22 @@ import {
 } from '~/features/keys-map';
 import type { RootState, AppDispatch } from '~/features/store';
 
+const getPitch = (pianoKey: PianoKey) => {
+  const { note, octave } = pianoKey;
+
+  return `${note}${octave}`;
+};
+
+const getOctaveRange = (keysMap: KeysMap) => {
+  const lowestPitch = getPitch(keysMap[0]);
+  const highestPitch = getPitch(keysMap[keysMap.length - 1]);
+
+  return `Note Range: ${lowestPitch} - ${highestPitch}`;
+};
+
 export const useOctaveButtons = () => {
   const keysMap = useSelector((state: RootState) => state.keysMap);
   const dispatch = useDispatch<AppDispatch>();
-
-  const getPitch = (pianoKey: PianoKey) => {
-    const { note, octave } = pianoKey;
-
-    return `${note}${octave}`;
-  };
-
-  const getOctaveRange = (keysMap: KeysMap) => {
-    const lowestPitch = getPitch(keysMap[0]);
-    const highestPitch = getPitch(keysMap[keysMap.length - 1]);
-
-    return `Note Range: ${lowestPitch} - ${highestPitch}`;
-  };
 
   const handleRaiseOctave = () => {
     dispatch(raiseOctave());
