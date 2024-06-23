90489c2d03a0d8f35b36a4ef7fc9e3dfb6db8dbb
Ashley Mays <ashleymays091@gmail.com>
2024-06-22 22:43:07 -0700

    fix (audio): use correct api route

--- client/src/features/audio/api.ts
+++ client/src/features/audio/api.ts
@@ -18,7 +18,7 @@ export const getAudioSamples = async (instrument: string) => {
 
 const fetchAudioSamples = async (instrument: string): Promise<ApiResponse> => {
   const response = await fetch(
-    `${import.meta.env.VITE_INSTRUMENT_API_URL}/${instrument}/audio`
+    `${import.meta.env.VITE_INSTRUMENT_API_URL}/instruments/${instrument}/audio`
   );
 
   return response.json();
