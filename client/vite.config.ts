93f6afd363f0f965441e5c2801672ef9454d5969
Ashley Mays <ashleymays091@gmail.com>
2024-06-27 23:01:17 -0700

    test: additional setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -16,6 +16,8 @@ export default defineConfig({
   },
   plugins: [react()],
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    globals: true
   },
 });
