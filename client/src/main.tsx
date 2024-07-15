96cf638c04b9ba87b89564c13865a1a4fe17db78
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2024-07-14 21:43:51 -0700

    feat: uncomment strictmode and rename file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-// import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { App } from './app';
@@ -6,9 +6,9 @@ import { store } from './store';
 import './styles/index.scss';
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
-  // </StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
