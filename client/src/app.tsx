dcd81a61f69a0f8ea0db81274cad8bc7ca46fdf3
Ashley Mays <ashleymays091@gmail.com>
2024-07-15 13:30:30 -0700

    refactor: move provider to app component

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,13 +1,17 @@
+import { Provider } from 'react-redux';
 import { Keyboard } from './components/keyboard';
 import { Toasts } from './components/toasts';
 import { Modal } from './components/modal';
+import { store } from './store';
 
 export const App = () => {
   return (
-    <main>
-      <Keyboard />
-      <Toasts />
-      <Modal />
-    </main>
+    <Provider store={store}>
+      <main>
+        <Keyboard />
+        <Toasts />
+        <Modal />
+      </main>
+    </Provider>
   );
 };
