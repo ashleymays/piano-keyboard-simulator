6d3752b249fafe8e0fc506bb099b60be836dfa29
Ashley Mays <ashleymays091@gmail.com>
2024-07-18 22:34:42 -0700

    refactor: move error throw to root function feat: add pitch validation

--- api/src/middlewares/invalid-route-handler.ts
+++ api/src/middlewares/invalid-route-handler.ts
@@ -2,5 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import type { Request, Response } from 'express';
 
 export const invalidRouteHandler = (req: Request, res: Response) => {
-  res.status(StatusCodes.BAD_REQUEST).json({ error: 'Invalid route requested' });
+  res
+    .status(StatusCodes.BAD_REQUEST)
+    .json({ error: 'Invalid route requested' });
 };
