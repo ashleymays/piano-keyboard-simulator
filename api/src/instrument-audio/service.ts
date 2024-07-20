8aba179b5e793ed34536cba4f5d3ebac28e0927a
Ashley Mays <ashleymays091@gmail.com>
2024-07-19 19:41:11 -0700

    refactor (api): add jsdoc and remove unnecessary variable

--- api/src/instrument-audio/service.ts
+++ api/src/instrument-audio/service.ts
@@ -35,14 +35,19 @@ const fetchInstrumentAudio = async (name: string) => {
   return response.data;
 };
 
+/**
+ *
+ * @param files - The files from the GitHub repository
+ * @returns an object that maps a pitch to an audio file url
+ */
 const createAudioMap = async (files: DirectoryFile[]) => {
   const audioFileLinks = getAudioFileLinks(files);
-  const pitches = getPitchesFromFileNames(files);
 
-  if (!hasCorrectFileCount(audioFileLinks) || !hasCorrectFileCount(pitches)) {
+  if (!areAllAudioFilesPresent(audioFileLinks)) {
     throw new Error('Could not get all audio files.');
   }
 
+  const pitches = getPitchesFromFileNames(files);
   const audioFilesInBase64 = await convertAudioToBase64(audioFileLinks);
 
   return mapPitchesToAudioFiles(pitches, audioFilesInBase64);
@@ -95,13 +100,25 @@ const isValidPitch = (pitch: string) => {
   );
 };
 
-const hasCorrectFileCount = (files: string[]) => {
-  // We check for 84 files because we don't use the notes `A0`, `Bb0`, `B0`, and `C7`.
-  const EXPECTED_NUMBER_OF_AUDIO_FILES = 84;
-
-  return files.length === EXPECTED_NUMBER_OF_AUDIO_FILES;
+/**
+ * Checks that all 84 audio files for an instrument were fetched.
+ *
+ * We check for 84 because we don't use the pitches `A0`, `Bb0`, `B0`, or `C7`,
+ * and there are 88 total pitches on a piano.
+ *
+ * @param files - either directory files or the specific audio file urls
+ */
+const areAllAudioFilesPresent = (files: string[]) => {
+  return files.length === 84;
 };
 
+/**
+ * Downloads the audio for an instrument and converts it to
+ * transferable base64 urls.
+ *
+ * @param audioFileLinks
+ * @returns The audio for the instrument in base 64 urls
+ */
 const convertAudioToBase64 = async (audioFileLinks: string[]) => {
   const arrayBuffers = await convertAudioFilesToArrayBuffers(audioFileLinks);
   const base64Files = convertArrayBuffersToBase64(arrayBuffers);
