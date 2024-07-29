442a78501d58ef56ed958a518b60ed6055477061
Ashley Mays <ashleymays091@gmail.com>
2024-07-28 22:05:16 -0700

    refactor: remove unnecessary type since redux issue was fixed

--- client/src/store/audio/reducer.ts
+++ client/src/store/audio/reducer.ts
@@ -1,9 +1,8 @@
 import { createAsyncThunk, createReducer } from '@reduxjs/toolkit';
 import { getAudioPlayers } from './api';
-import type { Reducer } from '@reduxjs/toolkit';
 import type { Players } from 'tone';
 
-export const loadAudio = createAsyncThunk<Players, string, any>(
+export const loadAudio = createAsyncThunk<Players, string>(
   'audio/load',
   (instrument) => getAudioPlayers(instrument)
 );
@@ -20,33 +19,16 @@ const initialState: AudioState = {
   error: null
 };
 
-/**
- * Taken from the Redux Toolkit source code.
- *
- * There is an issue with typing right now, so this is needed to get rid of
- * TS errors.
- *
- * @link https://github.com/reduxjs/redux-toolkit/pull/4467
- *
- * @todo Remove when the issue is resolved
- */
-type AudioReducer = Reducer<AudioState> & {
-  getInitialState: () => AudioState;
-};
-
-export const reducer: AudioReducer = createReducer<AudioState>(
-  initialState,
-  (builder) => {
-    builder.addCase(loadAudio.pending, (state) => {
-      state.isLoading = true;
-    });
-    builder.addCase(loadAudio.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.players = action.payload;
-    });
-    builder.addCase(loadAudio.rejected, (state, action) => {
-      state.isLoading = false;
-      state.error = action.error.message || null;
-    });
-  }
-);
+export const reducer = createReducer<AudioState>(initialState, (builder) => {
+  builder.addCase(loadAudio.pending, (state) => {
+    state.isLoading = true;
+  });
+  builder.addCase(loadAudio.fulfilled, (state, action) => {
+    state.isLoading = false;
+    state.players = action.payload;
+  });
+  builder.addCase(loadAudio.rejected, (state, action) => {
+    state.isLoading = false;
+    state.error = action.error.message || null;
+  });
+});
