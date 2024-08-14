0b2d3e5480f0cfb8ffd85a8ebaed0e97ad19311e
Ashley Mays <ashleymays091@gmail.com>
2024-08-14 02:33:10 -0700

    fix: use environment variable for proxy url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,24 +1,28 @@
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import type { UserConfig as VitestUserConfig } from 'vitest/config';
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~': '/src'
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+
+  return {
+    resolve: {
+      alias: {
+        '~': '/src'
+      },
     },
-  },
-  server: {
-    proxy: {
-      "/api": import.meta.env.VITE_INSTRUMENT_API_URL || "http://localhost:8080/api/v2", // dev url
-    }  
-  },
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    globals: false
-  },
+    server: {
+      proxy: {
+        '/api': process.env.VITE_INSTRUMENT_API_URL,
+      }  
+    },
+    plugins: [react()],
+    test: {
+      environment: 'jsdom',
+      setupFiles: './src/test/setup.ts',
+      globals: false
+    },
+  }
 });
