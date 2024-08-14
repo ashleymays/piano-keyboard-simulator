2f574c4e78bddfcdf244b85939fe160ad0cbbe06
Ashley Mays <ashleymays091@gmail.com>
2024-08-13 18:51:27 -0700

    fix: add dispatch middleware to response object and fix import statements

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { routes } from './routes.ts';
-import { globalErrorHandler } from './middlewares/global-error-handler.ts';
-import { invalidRouteHandler } from './middlewares/invalid-route-handler.ts';
+import { globalErrorHandler } from './middleware/global-error-handler.ts';
+import { invalidRouteHandler } from './middleware/invalid-route-handler.ts';
 
 export const app = express();
 
--- api/src/middleware/add-dispatch.ts
+++ api/src/middleware/add-dispatch.ts
@@ -1,7 +1,18 @@
 import type { Request, Response, NextFunction } from 'express';
 
-export const addDispatch = (req: Request, res: Response, next: NextFunction) => {
+/**
+ * Adds a function `dispatch` to the response object.
+ *
+ * This allows us to always send a status code and an object with property `data` in JSON format.
+ */
+export const addDispatch = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   res.dispatch = (statusCode: number, data: unknown) => {
     res.status(statusCode).json({ data });
   };
+
+  next();
 };
