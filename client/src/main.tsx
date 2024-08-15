dfa2dbe85dbd0d91f7f1f5ab9e408feaa81d7a85
Ashley Mays <ashleymays091@gmail.com>
2024-08-15 12:20:08 -0700

    feat: add vercel analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
+import { Analytics } from "@vercel/analytics/react";
 import './styles/index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>
 );
